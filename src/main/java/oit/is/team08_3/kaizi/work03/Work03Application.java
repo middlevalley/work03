4d1a155eb6330342c368c4b4d7736f8504c98bb5
middlevalley <e1b20074@oit.ac.jp>
2022-10-11 14:14:38 +0900

    初期化

---
+++ src/main/java/oit/is/team08_3/kaizi/work03/Work03Application.java
@@ -0,0 +1,13 @@
+package oit.is.team08_3.kaizi.work03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Work03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Work03Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team08_3/kaizi/work03/Work03ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team08_3.kaizi.work03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Work03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
