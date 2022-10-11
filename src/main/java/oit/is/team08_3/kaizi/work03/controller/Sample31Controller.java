0ad6f24e53768518954b505a6bed8773e675106b
b20089 <e1b20089@oit.ac.jp>
2022-10-11 17:06:53 +0900

    789

--- src/main/java/oit/is/team08_3/kaizi/work03/controller/Sample31Controller.java
+++ src/main/java/oit/is/team08_3/kaizi/work03/controller/Sample31Controller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//import oit.is.team08_3.kaizi.work03.model.Room;
+import oit.is.team08_3.kaizi.work03.model.Room;
 
 /**
  * /sample3へのリクエストを扱うクラス authenticateの設定をしていれば， /sample3へのアクセスはすべて認証が必要になる
@@ -19,8 +19,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/sample3")
 public class Sample31Controller {
 
-  // @Autowired
-  // private Room room;
+  @Autowired
+  private Room room;
 
   @GetMapping("step1")
   public String sample31() {
@@ -32,53 +32,48 @@ public class Sample31Controller {
     return "sample33.html";
   }
 
-  /*
-   * @GetMapping("step7")
-   * public String sample37() {
-   * return "sample37.html";
-   * }
-   *
-   * /**
+  @GetMapping("step7")
+  public String sample37() {
+    return "sample37.html";
+  }
+
+  /**
    *
    * @param model Thymeleafにわたすデータを保持するオブジェクト
-   *
-   * @param prin ログインユーザ情報が保持されるオブジェクト
-   *
+   * @param prin  ログインユーザ情報が保持されるオブジェクト
    * @return
    */
-  /**/
   @GetMapping("step2")
   public String sample32(ModelMap model, Principal prin) {
     String loginUser = prin.getName(); // ログインユーザ情報
     model.addAttribute("login_user", loginUser);
     return "sample31.html";
   }
-      @PostMapping("step6")
-      public String sample36(@RequestParam Integer hiku1, @RequestParam Integer
-      hiku2, ModelMap model) {
-      int kekka = hiku1 - hiku2;
-      model.addAttribute("hikukekka", kekka);
-      return "sample33.html";
-      }
-     /*
-     * @GetMapping("step8")
-     * public String sample38(Principal prin, ModelMap model) {
-     * String loginUser = prin.getName();
-     * this.room.addUser(loginUser);
-     * model.addAttribute("room", this.room);
-     *
-     * return "sample37.html";
-     * }
-     *
-     * @GetMapping("step9")
-     * public String sample39(Principal prin, ModelMap model) {
-     * String loginUser = prin.getName();
-     * Room newRoom = new Room();
-     * newRoom.addUser(loginUser);
-     * model.addAttribute("new_room", newRoom);
-     *
-     * return "sample37.html";
-     * }
-     */
+
+  @PostMapping("step6")
+  public String sample36(@RequestParam Integer hiku1, @RequestParam Integer hiku2, ModelMap model) {
+    int kekka = hiku1 - hiku2;
+    model.addAttribute("hikukekka", kekka);
+    return "sample33.html";
+  }
+
+  @GetMapping("step8")
+  public String sample38(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.room.addUser(loginUser);
+    model.addAttribute("room", this.room);
+
+    return "sample37.html";
+  }
+
+  @GetMapping("step9")
+  public String sample39(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    Room newRoom = new Room();
+    newRoom.addUser(loginUser);
+    model.addAttribute("new_room", newRoom);
+
+    return "sample37.html";
+  }
 
 }
